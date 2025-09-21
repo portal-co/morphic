e35d966ffd22959f8e4fef462fdd0d9b35e1dc03
Graham Kelly <gkgoat6700@gmail.com>
2025-09-21 22:08:10 +0000

    dep rollup

--- crates/portal-solutions-morphic-wasm/build.sh
+++ crates/portal-solutions-morphic-wasm/build.sh
@@ -2,7 +2,9 @@ cd $(dirname $0)
 set -e
 cargo install wasm-pack
 RUSTFLAGS="-C target-cpu=mvp" wasm-pack build -s portal-solutions . -Zbuild-std=std,panic_abort 
-sed -i.bak -e 's*"name": "@portal-solutions/portal-solutions-morphic-wasm-web"*"name": "@portal-solutions/morphic-wasm-web"*g' pkg/package.json
+sed -i.bak -e 's*"name": "@portal-solutions/portal-solutions-"*"name": "@portal-solutions/"*g' pkg/package.json
+mv pkg/package.json pkg/package.json.bak
+jq '.dependencies={"@portal-solutions/morphic-wasm-dep-rollup":"^1.0.0"}'  pkg/package.json.bak > pkg/package.json
 rm -rf pkg-wasm2js || true
 cp -r ./pkg ./pkg-wasm2js
 sed -i.bak -e 's*"name": "@portal-solutions/morphic-wasm-web"*"name": "@portal-solutions/morphic-wasm-web.wasm2js"*g' pkg-wasm2js/package.json
